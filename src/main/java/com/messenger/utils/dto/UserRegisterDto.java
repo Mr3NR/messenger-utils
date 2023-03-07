0a0f1fdf4c8f6ab25a0525a4f5706ab7d4b768ef
Mohsen <mohsenr7596@hotmail.com>
2023-03-07 14:24:16 +0330

    add user register dto

---
+++ src/main/java/com/messenger/utils/dto/UserRegisterDto.java
@@ -0,0 +1,10 @@
+package com.messenger.utils.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserRegisterDto {
+    private String username;
+}
